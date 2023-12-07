db626a74c6bfc431452174915be50a6c19dc44f9
ubyhzargam <ubyhzargam@gmail.com>
2023-12-07 23:07:29 +0530

    Update main.cpp

--- count_sort/main.cpp
+++ count_sort/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  count_sort
-//
-//  Created by Uby H on 07/12/23.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+int Max(int A[],int n)
+{
+    int max=A[0];
+    for(int i=1;i<n;i++)
+        if(A[i]>max)
+            max=A[i];
+    return max;
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void CountSort(int A[],int n)
+{
+    int i,j=0,max;
+    max=Max(A,n);
+    int *p=(int*)malloc(sizeof(int)*(max+1));
+    for(i=0;i<max+1;i++)
+        p[i]=0;
+    for(i=0;i<n;i++)
+        p[A[i]]++;
+    i=0;
+    while(i<=max)
+    {
+        if(p[i]!=0)
+        {
+            A[j++]=i;
+            p[i]--;
+        }
+        else
+            i++;
+    }
+}
+
+void Display(int A[],int n)
+{
+    cout<<"The sorted elements are given by : "<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+}
+
+int main()
+{
+    cout<<"Enter the number of elements in the array : "<<endl;
+    int n;
+    cin>>n;
+    int A[100];
+    cout<<"Enter the elements in the array : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>A[i];
+    CountSort(A,n);
+    Display(A,n);
     return 0;
 }
